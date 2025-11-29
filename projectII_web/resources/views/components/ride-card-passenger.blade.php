0f5155d419651ece00acb35546d37a28e10fa1f3
OsvaldoZunniga <osvaldo66239@gmail.com>
2025-11-28 23:22:34 -0600

    Se añade action a la card passenger para reservar ride

--- projectII_web/resources/views/components/ride-card-passenger.blade.php
+++ projectII_web/resources/views/components/ride-card-passenger.blade.php
@@ -6,8 +6,9 @@
 <div class="col-md-4 mb-4">
     @include('components.ride-card-base', [
         'ride' => $ride,
-        'cardAction' => '<form action="#" method="post" onsubmit="alert(\'Funcionalidad de reservas pendiente\'); return false;">
-                            <input type="hidden" name="ride_id" value="' . $rideData['idRide'] . '">
+        'cardAction' => '<form action="' . route('bookings.store') . '" method="POST">' .
+                            csrf_field() .
+                            '<input type="hidden" name="ride_id" value="' . $rideData['idRide'] . '">
                             <button type="submit" class="btn btn-success w-100">
                                 <i class="fas fa-check-circle me-2"></i> Reservar Ride
                             </button>
