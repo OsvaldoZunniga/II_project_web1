8a44e703bdc1669265888f7b5bcfe4a9d217f713
OsvaldoZunniga <osvaldo66239@gmail.com>
2025-11-28 22:32:59 -0600

    Funcion crear reserva added en BookingService

---
+++ projectII_web/app/Services/bookingService.php
@@ -0,0 +1,26 @@
+<?php
+
+    namespace App\Services;
+    use App\Models\Booking;
+    use App\Models\Ride;
+
+
+
+    class BookingService
+    {
+        /**
+         * Crear una nueva reserva
+         */
+        public function createBooking($idUsuario, $rideId): array
+        {
+            
+            // Crear la reserva
+            $booking = new Booking();
+            $booking->idUsuario = $idUsuario;
+            $booking->ride_id = $rideId;
+            $booking->save();
+
+            return ['success' => true, 'booking_id' => $booking->id];
+        }
+    }
+?>
